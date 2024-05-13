ea9d6f16fe5e7d7d81dc080fe183e4afbfb88cee
BogdanovOleksandr <168231566+BogdanovOleksandr@users.noreply.github.com>
2024-05-13 20:28:45 +0300

    Add files via upload

---
+++ lab09/prj/Software/ModulesBogdanov.h
@@ -0,0 +1,9 @@
+#ifndef MODULESBOGDANOV_H_INCLUDED
+#define MODULESBOGDANOV_H_INCLUDED
+
+float s_calculation(int, int, int);
+void printInfoTornado(float);
+void avgTemp(float[]);
+void countBits(int);
+
+#endif // MODULESBOGDANOV_H_INCLUDED
